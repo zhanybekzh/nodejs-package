58708a3c72233ba2be6018285dc6244750f9515d
Kirill Mokevnin <mokevnin@gmail.com>
2019-10-04 15:23:53 -0400

    update deps

--- src/half.js
+++ src/half.js
@@ -1,9 +1,12 @@
 // @ts-check
 
+import _ from 'lodash-es';
+
 /**
  * @param {Number} num
  */
 export default (num) => {
   const result = num / 2;
+  _.noop();
   return result;
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import 'source-map-support/register';
 import half from './half';
 
 export default half;
